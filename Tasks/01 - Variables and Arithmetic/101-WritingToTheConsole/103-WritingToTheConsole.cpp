a44d4c76a044cc6076d7909a855818bde68b384f
JosWheals <64151785+JosWheals@users.noreply.github.com>
2023-10-19 16:24:58 +0100

    Completing tasks
    completing all the tasks for the week 1 lab

--- Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
+++ Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
@@ -8,7 +8,7 @@ int main()
 
     // ************************ Using printf ******************************* 
     printf("Hello Everyone. We are using C\nHow are things?\n");
-    // TASK (a) - remove \n from the string above - what does this change? 
+    // TASK (a) - remove \n from the string above - what does this change? answer: it moves the next print statement "My age is ..." onto the same line as this one
     
     // ******************* Using printf placeholders *********************** 
     int age = 23;
@@ -17,6 +17,7 @@ int main()
     // Some other placeholders include %c for character and %f for floating point
     float radius = 10.0f;
     float area = 3.415926 * radius * radius;
+    printf("The Variable area %5.3f \n", area);
 
     // TASK(b) - Use printf to display the value of the variable area. Look at the lecture slides and find out how to limit this to 3 decimal places
     // Write solution here
@@ -27,6 +28,7 @@ int main()
     int p = 3;
     int q = 4;
     printf("p=%d, q=%d\n", p, q);
+    printf("Radius = %.3f, Area = %.3f", radius, area);
 
     // TASK(c) - using printf, display the radius and the area using a single printf statement
     // Write solution here
