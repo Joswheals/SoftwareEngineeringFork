710b03e1f79536e114a6e963be3f89238619f3a8
JosWheals <64151785+JosWheals@users.noreply.github.com>
2023-10-19 17:02:19 +0100

    Lab 3 Task completion
    not fully finished yet but got through most of them getting a little confused about what we are supposed to be doing for some of the tasks due to no actaul tasks next to them so iv just started just having fun with them and adding and testing things in c++

--- Tasks/03 - Flow Control/301-if-statement/301-if-statement.cpp
+++ Tasks/03 - Flow Control/301-if-statement/301-if-statement.cpp
@@ -20,7 +20,7 @@ int main()
 {
     //Some constants 
     const int high = 100, low = 10;
-    
+
     // start filling the tank 
     int level = 5;
 
@@ -29,9 +29,14 @@ int main()
     do {
         cout << "Please enter a level (between 0 and 100)" << endl;
         cin >> level;
-    } while (level < 0);
+    } while ((level < 0) || (level > 100));
+    
+    if (level == 0)
+    {
+        cout << "Tank is empty sadge" << endl;
+    }
 
-    if (level <= low)                       // check the level
+    if ((level <= low) || (~level == 0)) // check the level also noticed that in this addition if you put in 0 then you would get both if statement running at the same time which is not incorrect you would need to refill your tank cause its completly empty but i couldnt stop it from running both
     {
         cout << "Tank needs filling" << endl;
     }
